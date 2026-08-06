6988164e487de8ace2a507fb1f58d92cc48b80ac
Niyikiza250 <niyikizaeric250@gmail.com>
2026-08-06 11:06:24 +0200

    first commit

---
+++ Syntax and Variable/src/Main.java
@@ -0,0 +1,121 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        /*int num1;
+        System.out.println("Enter first number: ");
+        num1=sc.nextInt();
+        int num2;
+        System.out.println(" Enter second number: ");
+        num2=sc.nextInt();
+        int sum= num1+num2;
+        System.out.println(" the sum of two is " + sum);
+
+        String name;
+        String countName;
+        System.out.println("Enter your name inside here: ");
+        name=sc.nextLine();
+        System.out.println("Enter the name of you country: ");
+        countName=sc.nextLine();
+        System.out.println("My name is "+ name +" I'm from "+countName +" country");
+        System.out.println("I'm learning JAVA");
+        System.out.println("Java is Interesting today");
+        System.out.println("I must practice every day");
+        System.out.println("************************");
+
+        // variable declaration
+        String name;
+        int age;
+        String countName;
+        int ID;
+        String Department;
+        // in put method
+        System.out.println("enter your name here: ");
+        name=sc.nextLine();
+        System.out.println("enter your age: ");
+        age=sc.nextInt();
+        System.out.println("enter your Id number ");
+        ID=sc.nextInt();
+        sc.nextLine();
+        System.out.println(" enter your department name");
+        Department=sc.nextLine();
+        System.out.println("Enter the name of you country: ");
+        countName=sc.nextLine();
+        System.out.println("\n\n\n*************************");
+        System.out.println(" WELCOME TO JAVA TODAY");
+        System.out.println("*************************\n\n");
+        System.out.println("..............here we go \n\n");
+
+        System.out.println("******************************");
+        System.out.println(" ======== MY PROFILE =========");
+        System.out.println(" Name: "+ name);
+        System.out.println(" Age : " + age);
+        System.out.println(" Country : " + countName);
+        System.out.println("==============================");
+        System.out.println("******************************\n\n");
+
+        System.out.println("******* STUDENT CARD *********");
+        System.out.println(" Name       : "+ name);
+        System.out.println(" ID         : "+ID);
+        System.out.println(" Department : "+ Department);
+        System.out.println("*******************************\n\n");
+
+
+        System.out.println("\n\n====================================================");
+        System.out.println(" ABC Bank Management System");
+        System.out.println("====================================================");
+        System.out.println(" Welcome !\n Please Choose one of the following service \n\n");
+        System.out.println("1. Create Account");
+        System.out.println("2. Deposit Money");
+        System.out.println("3. Withdraw Money");
+        System.out.println("4. Check Balance ");
+        System.out.println("5. Exit\n\n");
+        System.out.println("===================================================\n\n");
+        System.out.println(" ==========================================");
+        System.out.println("         RESTAURANT MENU");
+        System.out.println("===========================================\n");
+        System.out.println("1. Fried ......................$5");
+        System.out.println("2. Chicken Pilau ..............$25");
+        System.out.println("3. chicken Burger .............$7");
+        System.out.println("4. Pizza ......................$10");
+        System.out.println("5. Juice ......................$2");
+        System.out.println("\n ----------------------------------------\n Thank you for visiting us!\n ====================================================");
+        System.out.println("    __");
+        System.out.println("   /  \\");
+        System.out.println("  /    \\");
+        System.out.println(" /______\\");
+        System.out.println(" |      |");
+        System.out.println(" | [  ] |");
+        System.out.println(" |______|");
+        */
+        System.out.println("=========================================");
+        System.out.println("    BANK MANAGEMENT SYSTEM     ");
+        System.out.println(" ========================================\n");
+        System.out.println(" system started\n");
+        System.out.println("1. Loading database ");
+        System.out.println("2. Checking server connection ");
+        System.out.println("3. Verifying security settings ");
+        System.out.println("4. Opening Customer portal\n");
+        System.out.println(" Customer Process\n");
+        System.out.println("5. customer login");
+        System.out.println("6. Account verification");
+        System.out.println("7. Transaction Processing");
+        System.out.println("8. Updating account balance ");
+        System.out.println("9. Generating receipt\n");
+        System.out.println("System Shutdown\n");
+        System.out.println("10. Saving Records");
+        System.out.println("11. Closing connection");
+        System.out.println("12. System stopped\n");
+        System.out.println("=============================================");
+
+
+
+
+
+
+
+
+    }
+}
