45c8f2771c6729361a41d251c5be27aca7bdc002
Niyikiza250 <niyikizaeric250@gmail.com>
2026-08-10 16:08:56 +0200

    nested

--- Syntax and Variable/src/IfStatement.java
+++ Syntax and Variable/src/IfStatement.java
@@ -111,7 +111,7 @@ public class IfStatement {
             System.out.println("Entered number must pe positive");
         }
 
-         */
+
             int age;
             String gender;
             int experience;
@@ -134,7 +134,151 @@ public class IfStatement {
         }
 
 
+
+        // Nested if Stage
+
+        String username;
+        String password;
+        String accountActive;
+        System.out.println("Your account is active? ");
+        accountActive=sc.nextLine();
+
+        if (accountActive.equalsIgnoreCase("yes")) {
+            System.out.println("Your Account is Active");
+            System.out.println("Enter your name");
+            username=sc.nextLine();
+            if(username.equalsIgnoreCase("Eric Niyikiza")){
+                System.out.println("your name is valid");
+
+                System.out.println("Enter your Password here please");
+                password= sc.nextLine();
+
+
+                if(password.equalsIgnoreCase("Eric@123")){
+                    System.out.println("Your password is correct ");
+                }
+                else{
+                    System.out.println("Your password is invalid");
+                }
+            }
+            else {
+                System.out.println("Your name is invalid, please enter the real one");
+            }
+        }
+        else {
+            System.out.println("your Account is not active");
+        }
+
+
+
+        // University Admission
+        int age ;
+        String Diploma;
+        double marks;
+
+        System.out.println("Enter your age here : ");
+        age= sc.nextInt();
+        sc.nextLine();
+        if (age>=18){
+            System.out.println(" Welcome University Admission partal");
+            System.out.println("Do you have Diploma  Yes/ No");
+            Diploma=sc.nextLine();
+            if (Diploma.equalsIgnoreCase("Yes")){
+                System.out.println("how many marks do you have on it?");
+                marks=sc.nextDouble();
+                if (marks>=70){
+                    System.out.println("Now you can have Admission\n\n Try to take your documents to campus tomorrow");
+                }
+                else {
+                    System.out.println("According to your marks "+marks+ ", You're not eligible to Apply on this University. \n sorry.\n Try another University");
+                }
+            }
+            else {
+                System.out.println(" You must have Diploma before you apply");
+            }
+        }
+        else {
+            System.out.println("You are not eligible to make Admission");
+        }
+
+         */
+        // 🏦 Bank Loan Approval
+            String accountActive, stableAccount;
+            String username, password;
+            int age;
+            double credit,loanAmount;
+        System.out.println("====================================");
+        System.out.println( " WELCOME TO THE BANK LOAN SYSTEM");
+        System.out.println("====================================\n\n");
+        System.out.println("Is your bankAccount active? Yes/ Now");
+        accountActive=sc.nextLine();
+        if (accountActive.equalsIgnoreCase("yes")){
+            System.out.println("Enter your full name: ");
+            username=sc.nextLine();
+            if (username.equalsIgnoreCase("Eric niyikiza")){
+                System.out.println("YOur identity verified");
+                System.out.println(" Enter you password");
+                password=sc.nextLine();
+                if (password.equalsIgnoreCase("Eric@123")){
+                    System.out.println(" Enter your Age here : ");
+                    age=sc.nextInt();
+                    sc.nextLine();
+
+                    if (age>=18){
+                        System.out.println("Do have stable income");
+                        stableAccount=sc.nextLine();
+                        if (stableAccount.equalsIgnoreCase("yes")){
+                            System.out.println(" Enter your credit you want ");
+                            credit=sc.nextInt();
+                            sc.nextLine();
+                            if (credit>=500 && credit<50001){
+                                System.out.println("Enter your Loan amount ");
+                                loanAmount=sc.nextInt();
+                                sc.nextLine();
+                                if (loanAmount<=credit){
+                                    System.out.println(" Your loan Approved Congratulations\n You gonna get message of approve soon");
+                                }
+                               else {
+                                    System.out.println(" you must save before to get loan please");
+                                }
+                            }
+                            else {
+                                System.out.println(" you have to increse your credit amount to get a loan");
+                            }
+
+                        }
+                        else {
+                            System.out.println(" You must have a stable income please!");
+                        }
+                    }
+                    else {
+                        System.out.println("You're under age");
+                    }
+                }
+                else {
+                    System.out.println(" You entered invalid password");
+                }
+
+            }
+            else {
+                System.out.println(" You entered invalid username");
+            }
+
+        }
+        else {
+            System.out.println("you're rejected to our bank loan system");
+        }
+
+
+
+
+
+        // LOOPS= Do, While, For.
+
+
     }
 
 
 }
+
+
